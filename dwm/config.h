40828e59297a2173b88d123f7b41b0cc54fdff82
windwalk-bushido <windwalk-bushido@protonmail.com>
2021-02-20 16:48:53 +0100

    Updated keyboard shortcut.

--- dwm/config.h
+++ dwm/config.h
@@ -132,7 +132,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
 };
 
 /* button definitions */
